669ebec90f9651b3c72192b10a8183746237be46
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-03-13 15:32:15 +0100

    ADd material theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.google.fonts)
     implementation(libs.androidx.appcompat)
