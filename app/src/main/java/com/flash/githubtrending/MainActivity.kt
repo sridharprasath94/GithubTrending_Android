527f7172b3ee87002aafa300df7e4def567afc83
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-03-14 22:48:49 +0100

    Update app bar configuration

--- app/src/main/java/com/flash/githubtrending/MainActivity.kt
+++ app/src/main/java/com/flash/githubtrending/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.flash.githubtrending.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,12 +15,16 @@ import dev.androidbroadcast.vbpd.viewBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private val navController by lazy {
         (supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.topAppBar)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
@@ -37,6 +44,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             }
         }
     }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
 }
 
 //
