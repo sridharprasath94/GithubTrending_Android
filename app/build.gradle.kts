c1fa065680a7c89cb54952897bebb00a35835f88
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-03-15 12:24:50 +0100

    Add paging library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.room.paging)
     implementation(libs.android.navigation.fragment)
     implementation(libs.android.navigation.ui)
     implementation(libs.androidx.browser)
