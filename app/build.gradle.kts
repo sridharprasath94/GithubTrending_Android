993e442690804d2c6ff6ebf04ddc8e38b415aef2
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-03-18 11:49:36 +0100

    Add paging compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.browser)
     implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.androidx.google.fonts)
